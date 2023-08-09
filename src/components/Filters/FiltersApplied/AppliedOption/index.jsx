3b61a33c5cccdde004eb801064dea940627052ae
Iuliana Gugeanu <iugu0602@createq.space>
2023-08-09 16:41:40 +0300

    Cleaned out the code in filters.

--- src/components/Filters/FiltersApplied/AppliedOption/index.jsx
+++ src/components/Filters/FiltersApplied/AppliedOption/index.jsx
@@ -3,19 +3,16 @@ import { useState, useContext } from 'react';
 import { FiltersContext } from '@/components/Context/FiltersContext';
 
 export default function FilterApplied({ text, items, id }) {
-    const [isCancelled, setIsCancelled] = useState(false);
 
     const { removeFilter } = useContext(FiltersContext);
     const handleClick = event => {
         removeFilter({ text, items, id });
-        setIsCancelled(current => !current);
     }
     return (
         <button
             id={id}
             type="button"
-            className={`${styles.appliedFilter} 
-            ${isCancelled === true ? styles.isCancelled : ""}`}
+            className={`${styles.appliedFilter}`}
             onClick={handleClick}
         >
             🥖 {text}
