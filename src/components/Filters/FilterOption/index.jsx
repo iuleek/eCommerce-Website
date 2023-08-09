1639c16ca8ec2e59314ed35d449c4f4a3231c8e5
Iuliana Gugeanu <iugu0602@createq.space>
2023-08-09 11:48:13 +0300

    Solved some bugs about functionality of filters. Implemented 3 actual filters for product-listing.

--- src/components/Filters/FilterOption/index.jsx
+++ src/components/Filters/FilterOption/index.jsx
@@ -3,21 +3,22 @@ import { useContext, useState } from 'react';
 import { FiltersContext } from '@/components/Context/FiltersContext';
 
 export default function FilterOption({ text, items, id }) {
-    const [isActive, setIsActive] = useState(false);
 
-    const { filters, addFilter } = useContext(FiltersContext);
+    const { filters, addFilter, removeFilter } = useContext(FiltersContext);
+    const item = filters.find((filter) => {
+        return filter.id === id;
+    });
     const handleFilterClick = event => {
-        const item = filters.find((filter) => {
-            return filter.id === id;
-        });
         if (item === undefined) {
             addFilter({ text, items, id });
-            setIsActive(current => !current);
+        }
+        if (item !== undefined) {
+            removeFilter({ text, items, id });
         }
     }
 
     return (
-        <button id={id} type="button" className={`${styles.filterBtn} ${isActive === true ? styles.isActive : ""}`}
+        <button id={id} type="button" className={`${styles.filterBtn} ${item !== undefined ? styles.isActive : ""}`}
             onClick={handleFilterClick}>🥖 {text} {items} </button>
     );
 }
--- src/components/ProductListingComp/Products/index.jsx
+++ src/components/ProductListingComp/Products/index.jsx
@@ -1,21 +1,46 @@
 import ProductItem from '@/components/ProductItem'
 import styles from './Products.module.css'
 import data from './product-listing.json'
+import { FiltersContext } from '@/components/Context/FiltersContext';
+import { useContext } from 'react';
 
-export default function Products(props) {
-    const { id, img, discount, title, originalPrice, currPrice } = props;
-
+export default function Products() {
+    const { filters } = useContext(FiltersContext);
+    let labels = [];
+    filters && filters.map((filter) => {
+        labels.push(filter.text.toLowerCase());
+    })
     return (
         <div className={styles.products}>
-            {data.productListing.map((card, index) => (
-                <ProductItem key={index}
-                    id={card.id}
-                    img={card.img}
-                    title={card.name}
-                    originalPrice={card.price}
-                    currPrice={card.price}
-                    discount={card.discount} />
-            ))}
+            {labels.length !== 0 ? (
+                labels.map(label => (                   
+                        data.productListing.map((card, index) =>
+                            card.category === label && (
+                                <ProductItem
+                                    key={index}
+                                    id={card.id}
+                                    img={card.img}
+                                    title={card.name}
+                                    originalPrice={card.price}
+                                    currPrice={card.price}
+                                    discount={card.discount}
+                                />
+                            )
+                        )
+                ))
+            ) : (
+                data.productListing.map((card, index) => (
+                    <ProductItem
+                        key={index}
+                        id={card.id}
+                        img={card.img}
+                        title={card.name}
+                        originalPrice={card.price}
+                        currPrice={card.price}
+                        discount={card.discount}
+                    />
+                ))
+            )}
         </div>
-    )
+    );
 }
